067215cbdc366fc048d5bc75590a6aa25ebcad55
czz_zhenzhen <49966854+czz-zhenzhen@users.noreply.github.com>
2019-12-03 22:46:40 +0800

    czz 修改文档

--- DV_link_Test/Report_message/1111.py
+++ DV_link_Test/Report_message/1111.py
@@ -11,9 +11,9 @@ t1 = time.time()
 #
 # print(t2-t1)
 
-db = pymysql.connect('101.201.73.80','dvlink','Lrdata_2019',db='dvlink')
-cursor = db.cursor()
-sql = """insert into ATT_LOG(SEQ_ID,RESOURCE_ID,OP_NAME,OP_INPUT,OP_OUTPUT,START_DATE,END_DATE,OP_STATUS,OP_DESC) values (2,'2','操作步骤','操作输入','操作输出',sysdate(),sysdate(),1,'描述')"""
-cursor.execute(sql)
-db.commit()
-db.close()
+# db = pymysql.connect('101.201.73.80','dvlink','Lrdata_2019',db='dvlink')
+# cursor = db.cursor()
+# sql = """insert into ATT_LOG(SEQ_ID,RESOURCE_ID,OP_NAME,OP_INPUT,OP_OUTPUT,START_DATE,END_DATE,OP_STATUS,OP_DESC) values (2,'2','操作步骤','操作输入','操作输出',sysdate(),sysdate(),1,'描述')"""
+# cursor.execute(sql)
+# db.commit()
+# db.close()
